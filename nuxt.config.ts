66d3398e0d5ce69251f2e3575684371e492f8d51
kundakchan <kundakchan@yandex.ru>
2023-09-22 15:16:49 +0300

    start project

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import eslintPlugin from 'vite-plugin-eslint'
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      eslintPlugin()
+    ]
+  },
+  runtimeConfig: {
+    FIREBASE_CONNECTION: {
+      apiKey: process.env.FIREBASE_API_KEY
+    },
+    public: {
+      FIREBASE_CONNECTION: {
+        apiKey: process.env.FIREBASE_API_KEY
+      }
+    }
+  }
+})
